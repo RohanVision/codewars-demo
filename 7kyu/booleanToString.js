350db9c43bcad78fdca50deb3c94f00603ef59a1
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-09-16 17:51:20 -0400

    16/09/24

---
+++ 7kyu/booleanToString.js
@@ -0,0 +1,10 @@
+function booleanToString(b) {
+    if (b === true) return "true"
+    if (b === false) return "false"
+}
+
+
+// second approach
+function booleanToString(b) {
+    return b ? 'true' : 'false';
+}

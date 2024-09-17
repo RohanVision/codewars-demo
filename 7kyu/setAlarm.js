169a37c61476e75eab8e639db9e411d89286f199
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-09-17 10:41:18 -0400

    17/09/24

---
+++ 7kyu/setAlarm.js
@@ -0,0 +1,28 @@
+// Write a function named setAlarm/set_alarm/set-alarm/setalarm (depending on language) which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+
+// The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples:
+
+// employed | vacation 
+// true     | true     => false
+// true     | false    => true
+// false    | true     => false
+// false    | false    => false
+
+
+// function setAlarm(employed, vacation){
+//   if(employed === true && vacation === true) {
+//       return false;
+//   } else if (employed === false && vacation === true){
+//       return false;
+//   } else if (employed === false && vacation === false){
+//       return false
+//   } else {
+//       return true
+//   }
+// }
+
+const setAlarm = (employed, vacation) => {
+    return employed === true && vacation === true ? false :
+        employed === false && vacation === true ? false :
+            employed === false && vacation === false ? false : true
+}

ef74c9248dfe50abb9bb6caacfbd09361ac6a2db
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-09-25 09:50:07 -0400

    25/09/2024

---
+++ 6kyu/printNelementsArray.js
@@ -0,0 +1,15 @@
+
+
+
+function retrive(arr, n = 1) {
+    if (n <= arr.length) {
+        for (let i = 0; i < n; i++) {
+            console.log(arr[i]);
+        }
+    } else {
+        console.log(n + "elements value too big")
+    }
+}
+
+
+retrive([1, 2, 3, 4, 5], 3)

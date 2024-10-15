195ad58bd475f6b06ed40be782a858468b35b635
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-10-15 11:03:16 -0400

    ranger SUm query practice

--- leetcode/rangeSumQuery.js
+++ leetcode/rangeSumQuery.js
@@ -37,27 +37,49 @@
 /**
  * @param {number[]} nums
  */
-var NumArray = function (nums) {
 
-    this.prefixSum = new Array(nums.length + 1).fill(0);
 
-    for (let i = 0; i < nums.length; i++) {
-        this.prefixSum[i + 1] = this.prefixSum[i] + nums[i]
+class NumArray {
+    constructor(nums) {
+
+        this.prefixSum = new Array(nums.length + 1).fill(0);
+
+        for (let i = 0; i < nums.length; i++) {
+            this.prefixSum[i + 1] = this.prefixSum[i] + nums[i];
+        }
+
     }
+    /**
+     * @param {number} left
+     * @param {number} right
+     * @return {number}
+     */
+    sumRange(left, right) {
+        return this.prefixSum[right + 1] - this.prefixSum[left];
+    }
+}
 
-};
 
-/** 
- * @param {number} left 
- * @param {number} right
- * @return {number}
- */
-NumArray.prototype.sumRange = function (left, right) {
-    return this.prefixSum[right + 1] - this.prefixSum[left]
-};
-
-/** 
+/**
  * Your NumArray object will be instantiated and called as such:
  * var obj = new NumArray(nums)
  * var param_1 = obj.sumRange(left,right)
  */
+
+
+
+
+
+
+// Practice
+// const NumArray = function (nums) {
+//     this.prefixSum = new Array(nums.length + 1).fill(0);
+
+//     for (let i = 0; i < nums.length; i++) {
+//         this.prefixSum[i + 1] = this.prefixSum[i] + nums[i]
+//     }
+// }
+
+// NumArray.prototype.sumRange = function (left, right) {
+//     return this.prefixSum[right + 1] - this.prefixSum[left]
+// }

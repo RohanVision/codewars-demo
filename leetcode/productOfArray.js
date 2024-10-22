3fdfc0fbf5cb76c663d27213a34c3df5283500db
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-10-22 09:29:11 -0400

    product of array explanation

--- leetcode/productOfArray.js
+++ leetcode/productOfArray.js
@@ -5,3 +5,36 @@ function productOfArray(arr) {
     if (arr.length === 0) return 1;
     return arr.pop() * productOfArray(arr);
 }
+
+
+/*
+
+The function first checks base case if arr it if empty or not if it is return 1.
+if not then it recursivly call itself. 
+first it pop the last number from the array using pop() method and then it multiply with arr.
+
+example productOfArray(3)
+
+first call : productOfArray([1,2,3])
+its checks the length is not 0 so we proceed to arr.pop() and 3 return
+3 * productOfArray([1,2])
+
+second call : productOfArray([1,2])
+its checks the length is not 0 so we proceed to arr.pop() and 2 return
+2 * productOfArray([1])
+
+third call : productOfArray([1])
+its checks the length is not 0 so we proceed to arr.pop() and 1 return
+1 * productOfArray([])
+
+fourth call: productOfArray([])
+so base case reached because array is empty
+so it return 1
+
+now Backtracking and multiplying
+fourth call : returns 1
+third call: 1 * 1 = 1;
+second call:  2 * 1 = 2;
+first call : 3 * 2 = 6
+
+*/

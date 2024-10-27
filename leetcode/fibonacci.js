7c3ef2e26ad17072c141564963489e0186c27cdf
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-10-27 19:17:28 -0400

    Practice weekend

--- leetcode/fibonacci.js
+++ leetcode/fibonacci.js
@@ -12,17 +12,16 @@
 
 
 // Example 1:
-
 // Input: n = 2
 // Output: 1
 // Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
-// Example 2:
 
+// Example 2:
 // Input: n = 3
 // Output: 2
 // Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
-// Example 3:
 
+// Example 3:
 // Input: n = 4
 // Output: 3
 // Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
@@ -35,11 +34,13 @@
 
 // By Recurrsion
 
-var fib = function (n) {
+
+function fib(n) {
     if (n === 1) return 1;
     if (n === 0) return 0;
     return fib(n - 1) + fib(n - 2)
-};
+}
+
 
 
 const fib = function (num) {

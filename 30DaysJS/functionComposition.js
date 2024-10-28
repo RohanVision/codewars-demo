3d4ca94ec3b63ec5826161fa9272d80c77ad3426
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-10-28 11:36:48 -0400

    30 Days JS Function Composition Simple Solution

--- 30DaysJS/functionComposition.js
+++ 30DaysJS/functionComposition.js
@@ -71,3 +71,13 @@ var compose = function (functions) {
  * const fn = compose([x => x + 1, x => 2 * x])
  * fn(4) // 9
  */
+
+// for-loop simple
+var compose = function (functions) {
+    return function (x) {
+        for (let i = functions.length - 1; i >= 0; i--) {
+            x = functions[i](x)
+        }
+        return x;
+    }
+};

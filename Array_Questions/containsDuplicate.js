612d59cc0f9302a5b24a7ba139c24994af7d9b61
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-11-27 10:31:04 -0500

    Array Based Question

---
+++ Array_Questions/containsDuplicate.js
@@ -0,0 +1,72 @@
+// 217. Contains Duplicate
+// Solved
+// Easy
+// Topics
+// Companies
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+
+// Output: true
+
+// Explanation:
+
+// The element 1 occurs at the indices 0 and 3.
+
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+
+// Output: false
+
+// Explanation:
+
+// All elements are distinct.
+
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+// Output: true
+
+
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+// var containsDuplicate = function(nums) {
+//     nums.sort((a,b) => a - b);
+
+//     for(let i = 1; i < nums.length; i++){
+//         if(nums[i] === nums[i - 1]){
+//             return true;
+//         }
+//     }
+//     return false;
+// };
+
+// second approach with SET
+
+var containsDuplicate = function (nums) {
+    const numSet = new Set();
+
+    for (const n of nums) {
+        if (numSet.has(n)) {
+            return true;
+        } else {
+            numSet.add(n)
+        }
+    }
+    return false;
+};

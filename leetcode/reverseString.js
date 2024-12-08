8cf2c775b8d65d3a3f2ba36930760d99ae4776e9
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-12-08 15:40:57 -0500

    Sunday practice

--- leetcode/reverseString.js
+++ leetcode/reverseString.js
@@ -1,22 +1,19 @@
 // 344. Reverse String
 
-// Hint
 // Write a function that reverses a string. The input string is given as an array of characters s.
 
 // You must do this by modifying the input array in-place with O(1) extra memory.
 
 // Example 1:
-
 // Input: s = ["h","e","l","l","o"]
 // Output: ["o","l","l","e","h"]
-// Example 2:
 
+// Example 2:
 // Input: s = ["H","a","n","n","a","h"]
 // Output: ["h","a","n","n","a","H"]
 
 
 // Constraints:
-
 // 1 <= s.length <= 105
 // s[i] is a printable ascii character.
 
@@ -31,14 +28,18 @@ function reverse(s) {
         i++;
         j--;
     }
+    return s;
 }
 
-var reverseString = function (s) {
-    let i = 0;
-    let j = s.length - 1;
-    while (i <= j) {
-        [s[i], s[j]] = [s[j], s[i]]
-        i++;
-        j--
-    }
-};
+// function reverse(s) {
+//     i = 0;
+//     j = s.length - 1;
+//     while (i <= j) {
+//         [s[i], s[j]] = [s[j], s[i]];
+//         i++;
+//         j--;
+//     }
+// }
+
+console.log(reverse(["H", "a", "n", "n", "a", "h"]))
+console.log(reverse(["h", "e", "l", "l", "o"]))

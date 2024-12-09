3a98628654627554bc409972d31dd68da3eb1507
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-12-09 09:07:20 -0500

    Sunday practice

--- leetcode/palindrome.js
+++ leetcode/palindrome.js
@@ -3,27 +3,23 @@
 // Given a string s, return true if it is a palindrome, or false otherwise.
 
 
-
 // Example 1:
-
 // Input: s = "A man, a plan, a canal: Panama"
 // Output: true
 // Explanation: "amanaplanacanalpanama" is a palindrome.
-// Example 2:
 
+// Example 2:
 // Input: s = "race a car"
 // Output: false
 // Explanation: "raceacar" is not a palindrome.
-// Example 3:
 
+// Example 3:
 // Input: s = " "
 // Output: true
 // Explanation: s is an empty string "" after removing non-alphanumeric characters.
 // Since an empty string reads the same forward and backward, it is a palindrome.
 
-
 // Constraints:
-
 // 1 <= s.length <= 2 * 105
 // s consists only of printable ASCII characters.
 
@@ -38,10 +34,9 @@ function checkPal(s) {
         } else {
             return false;
         }
-        return true;
     }
+    return true;
 }
-
 
 var isPalindrome = function (s) {
     const regx = /[^a-zA-Z]/g;
@@ -49,18 +44,40 @@ var isPalindrome = function (s) {
     return checkPal(clean)
 };
 
+
+function checkPal(s) {
+    let i = 0;
+    let j = s.length - 1;
+    while (i <= j) {
+        if (s[i] === s[j]) {
+            i++;
+            j--;
+        }
+        return false;
+    }
+    return true;
+}
+
+const isPalindrome = function (s) {
+    const regx = /[^A-Za-z]/g;
+    const clean = s.replace(regx, "").toLowerCase();
+    return checkPal(clean)
+}
+
 // second Approach
 
-// var isPalindrome = function(s) {
-//     const result = s.replace(/[\W_]/g, "").toLowerCase();
-//     let l = 0;
-//     let r = result.length - 1;
-//     while(l < r){
-//         if(result[l] !== result[r]){
-//             return false;
-//         }
-//         l++;
-//         r--
-//     }
-//     return true;
-// };
+var isPalindrome = function (s) {
+    const result = s.replace(/[\W_]/g, "").toLowerCase();
+    let l = 0;
+    let r = result.length - 1;
+    while (l < r) {
+        if (result[l] !== result[r]) {
+            return false;
+        }
+        l++;
+        r--
+    }
+    return true;
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"))

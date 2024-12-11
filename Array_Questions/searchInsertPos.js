ae557bfb3e3205983282837baaae9a47e4c2bc55
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-12-11 09:00:34 -0500

    Practice

--- Array_Questions/searchInsertPos.js
+++ Array_Questions/searchInsertPos.js
@@ -52,6 +52,24 @@ var searchInsert = function (nums, target) {
     }
     return left;
 };
+var searchInsert = function (nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
+}
+console.log(searchInsert([1, 3, 5, 6], 2))
+console.log(searchInsert([1, 3, 5, 6], 7))
 
 /*
 Intuition

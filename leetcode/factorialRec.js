55a4b31ce66503b37b3797d40325940ab0d7d512
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-12-23 14:10:22 -0500

    Back to Practice

--- leetcode/factorialRec.js
+++ leetcode/factorialRec.js
@@ -1,8 +1,7 @@
 
 
-function factorial(num) {
-    if (num === 1) return 1;
-    return num * factorial(num - 1)
+function factorial(n) {
+    if (n === 1) return 1;
+    return n * factorial(n - 1);
 }
-
-console.log(factorial(4))
+console.log(factorial(4));

06ed2890ddb9fbdf4f2fbee47ef60ce525ef1988
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-12-23 14:04:54 -0500

    Back to Practice

--- leetcode/composition_pipe.js
+++ leetcode/composition_pipe.js
@@ -1,10 +1,11 @@
 // 11. what is Composition? create a pipe()
 
+
 function pipe(funcs) {
     return function (args) {
         let result = args;
         for (let fn of funcs) {
-            result = fn.call(this, result)
+            result = fn.call(this, result);
         }
         return result;
     }

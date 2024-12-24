8770d2bcbd8a5111d7d6b661660c74b5d17ead62
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-12-24 10:34:41 -0500

    Back to Practice

--- leetcode/palidromeRecurrsion.js
+++ leetcode/palidromeRecurrsion.js
@@ -1,8 +1,20 @@
+// function pal(str) {
+//     if (str.length === 1) return true
+//     if (str.length === 2) return str[0] === str[1]
+//     if (str[0] === str.slice(-1)) return pal(str.slice(1, -1))
+//     return false;
+// }
+// console.log(pal("tacocat"))
+// console.log(pal('amanaplanacanalpanama'));
+
 function pal(str) {
-    if (str.length === 1) return true
-    if (str.length === 2) return str[0] === str[1]
-    if (str[0] === str.slice(-1)) return pal(str.slice(1, -1))
-    return false;
+    // Base Case
+    if (str.length === 1) return true;
+    // if str is 2 letter then compare first and second letter
+    if (str.length === 2) return str[0] === str[1];
+    // recursive call compare first index with last if match remove first and last index help of slice method
+    if (str[0] === str.slice(-1)) return pal(str.slice(1, -1));
+    return false; // if not match reture false
 }
 console.log(pal("tacocat"))
-console.log(pal('amanaplanacanalpanama'))
+console.log(pal('amanaplanacanalpanama'));
